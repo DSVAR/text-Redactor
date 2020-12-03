aa1dd85fab4f7ecc217c90f007f0ecd52bfeaa62
DVAR <danilka070103@gmail.com>
2020-12-03 06:02:17 +0300

    1

--- Logical/FindSum.cs
+++ Logical/FindSum.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Logical
 {
@@ -9,24 +10,50 @@ namespace Logical
         private string[] Text;
         int Empty;
         int NextSim;
-
+        string ALF = @"[йцукенгшщзхъфывапролджэячсмитьбю]";
+        string Soglass = @"[цкнгшщхфвпрлджчсмтб]";
         public string Find(string text)
         {
-            for(int o=0; o < text.Length; o++)
+            Text = new string[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                Text[i] = text[i].ToString();
+            }
+
+
+            for (int o=0; o < Text.Length; o++)
             {
                 if (text[o].ToString()==" ")
                 {
-                    NextSim = i++;
-                    Empty++;
+                    NextSim = o+1;
+                    if (o != Text.Length) { 
+
+                        if (!Regex.IsMatch(Text[NextSim], ALF))
+                        {
+                            text[o].ToString().Replace(" ", "");
+                        }
+                        else
+                        {
+                            Empty++;
+                        }
+                    }
+                    else
+                    { 
+                        if (!Regex.IsMatch(Text[o], ALF))
+                        {
+                        text[o].ToString().Replace(" ", "");
+                        }
+                        else
+                        {
+                            Empty++;
+                        }
+                    }
                 }
             }
             
             Text = new[] { text };
-            for (int i = 0; i < text.Length; i++)
-            {
-
-            }
-
+         
             return text;
         }
     }
