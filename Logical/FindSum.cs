0fc896ea13e65daca59882cb7f426813eb2bac62
DVAR <danilka070103@gmail.com>
2020-12-04 05:58:02 +0300

    вместо Check поставить List<int> ,найти пробелы и считать от туда буквы и быть как LUDACRIS

--- Logical/FindSum.cs
+++ Logical/FindSum.cs
@@ -9,14 +9,19 @@ namespace Logical
     {
         private string[] Text;
         bool Empty = false;
-
-        string ObTest = null;
+        List<string> EndWords = new List<string>();
+        List<int> Points = new List<int>();
+        string ObTest,MtoB = null;
         string ALF = @"[йцукенгшщзхъфывапролджэячсмитьбю]";
         string Soglass = @"[цкнгшщхфвпрлджчсмтб]";
+
+        int Check ;
         public string Find(string text)
         {
-          
-            
+            MtoB = null;
+            Empty = false;
+            Check = 0;
+            EndWords.Clear();
             for (int i = 0; i < text.Length; i++)
             {
 
@@ -35,15 +40,50 @@ namespace Logical
                 }
 
             }
-            //Text = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            //Text = ObTest.Split(new char[] { ' '  });
+
+            Text = new string[ObTest.Length];
+            Empty = false;
 
             for (int o = 0; o < ObTest.Length; o++)
             {
+                
                 Text[o] = ObTest[o].ToString();
             }
+            //
+            if(Text[Text.Length-1]==" " || Text[Text.Length-1] ==null)
+            {
+                Text[Text.Length-1].Replace(Text[Text.Length-1], ".");
+            }
 
+            for (int g = 0; g < Text.Length; g++)
+            {
+                if (Text[g] == " ")
+                {
+                    Empty = true;
+                    Check = g;
+                }
 
+                if (Check > 0)
+                {
+                    for(int m = Check-3; m < Check; m++)
+                    {
+                        MtoB += text[m];
+                    }
+                    MtoB += " ";
+
+                    if(Check<=Text.Length-3)
+                        for (int m = Check ; m < Check+3; m++)
+                        {
+                            MtoB += text[m];
+                        }
+
+                    else
+                        for (int m = Check; m < Text.Length; m++)
+                        {
+                            MtoB += text[m];
+                        }
+                }
+            }
 
             Text = new[] { text };
          
